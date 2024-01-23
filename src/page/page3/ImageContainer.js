5d6258619953a5d5d2f7532c1d23b97db5757138
Abdulwasim-S <abdulwasimsmech@gmail.com>
2024-01-23 19:11:09 +0530

    update

--- src/page/page3/ImageContainer.js
+++ src/page/page3/ImageContainer.js
@@ -1,9 +1,40 @@
 import React from "react";
 import "./ImageContainer.css";
-import { Box } from "@chakra-ui/react";
-
+import { Box, Grid, GridItem, Image } from "@chakra-ui/react";
+import card_bg from "./box .png";
 const ImageContainer = () => {
-  return <Box></Box>;
+  return (
+    <Box w={"100%"}>
+      <Grid
+        display={"flex"}
+        justifyContent={"space-between"}
+        templateColumns="repeat(5, 1fr)"
+        gap={4}
+      >
+        <GridItem colSpan={1}>
+          <Image src={card_bg} alt="img" />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Image src={card_bg} alt="img" />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Image src={card_bg} alt="img" />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Image src={card_bg} alt="img" />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Image src={card_bg} alt="img" />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Image src={card_bg} alt="img" />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Image src={card_bg} alt="img" />
+        </GridItem>
+      </Grid>
+    </Box>
+  );
 };
 
 export default ImageContainer;
--- src/page/page3/ThirdPage.js
+++ src/page/page3/ThirdPage.js
@@ -1,25 +1,26 @@
 import React from "react";
 import "./ThirdPage.css";
 import ImageContainer from "./ImageContainer";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 const ThirdPage = () => {
   return (
-    <div className="third-page">
-      <h1 className="text-heading">
+    <Box className="third-page">
+      <Heading className="text-heading">
         Our <span className="text-cyan">Services</span>
-      </h1>
-      <p className="text-normal">
+      </Heading>
+      <Text className="text-normal">
         XR can improve worker training by simulating complex processes,
         increasing safety, and reducing the risk of errors. XR can also assist
         in product design and prototyping, enhancing collaboration, and reducing
         development time. Additionally, XR can provide remote support, enabling
         experts to assist on-site technicians in real-time, improving
         efficiency, and reducing downtime.
-      </p>
-      <div className="image-container">
+      </Text>
+      <Box className="image-container">
         <ImageContainer />
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 };
 
