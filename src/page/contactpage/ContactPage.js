3bac2ddce333b3484c1c9bf4ff7f5aa28f7caf53
Abdulwasim-S <abdulwasimsmech@gmail.com>
2024-01-25 14:38:40 +0530

    update

--- src/page/contactpage/ContactPage.js
+++ src/page/contactpage/ContactPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./ContactPage.css";
-import { Heading, Link } from "@chakra-ui/react";
+import { Box, Heading, Link } from "@chakra-ui/react";
 
 const ContactPageComp = () => {
   return (
@@ -15,9 +15,16 @@ const ContactPageComp = () => {
         We build Data-Driven, Customer Centric XR applications to achieve your
         business objectives
       </Heading>
+      <Heading
+        m={{ base: "3vh 4vw" }}
+        fontSize={{ base: "lg", md: "xl", lg: "3xl" }}
+        color={"white"}
+      >
+        Contact Us
+      </Heading>
       <div className="contact-us-address text-white row">
         <div className="contact-us-address-mainbox col-lg-4">
-          <h3 className="contact-us-address-heading">Contact Us</h3>
+          {/* <h3 className="contact-us-address-heading">Address 1</h3> */}
           <p className="contact-us-address-para">
             WOCO SPACES, 4/222, East Coast Rd, Palavakkam, Chennai, Tamil Nadu
             600041, <br />
@@ -31,10 +38,12 @@ const ContactPageComp = () => {
           <div className="contact-page-hiddden-border"></div>
         </div>
         <div className="contact-us-address-mainbox col-lg-3">
+          {/* <Box display={{ base: "none", lg: "block" }}>
           <h3 className="contact-us-address-heading">
-            {" "}
+            Address 2
             <br />
           </h3>
+          </Box> */}
           <p className="contact-us-address-para">
             110-104 Crockford Blvd, Toronto, ON,M1R 3C3. <br />
             Phone :{" "}
@@ -47,7 +56,7 @@ const ContactPageComp = () => {
           <div className="contact-page-hiddden-border"></div>
         </div>
         <div className="contact-us-address-mainbox col-lg-3">
-          <h3 className="contact-us-address-heading">Email</h3>
+          {/* <h3 className="contact-us-address-heading">Email</h3> */}
           <p className="contact-us-address-para">
             <Link href="mailto:support@meynikara.com" target="_blank">
               support@meynikara.com
