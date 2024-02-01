7120f03d06809a5903badfd87074cb16384be272
Abdulwasim-S <abdulwasimsmech@gmail.com>
2024-02-01 14:59:02 +0530

    update

--- src/page/navbar/NavPage.js
+++ src/page/navbar/NavPage.js
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import "./NavPage.css";
 import {
-  Box,
   Button,
   Drawer,
   DrawerBody,
@@ -11,18 +10,19 @@ import {
   DrawerHeader,
   DrawerOverlay,
   Flex,
+  Heading,
   Image,
-  Input,
+  Link,
   useDisclosure,
 } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
 import logo_img from "./image/logo.png";
 
 const NavPage = () => {
-  const [show, setShow] = useState(false);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const btnRef = React.useRef();
+  const [show, setShow] = useState(false);
   return (
     <Flex
       position={"relative"}
@@ -198,14 +198,117 @@ const NavPage = () => {
           </div>
         </div>
       </nav>
-      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+      <Drawer
+        size={{ base: "xs", md: "md" }}
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+      >
         <DrawerOverlay />
-        <DrawerContent>
+        <DrawerContent bg={"#1b1a1a"} color={"white"}>
           <DrawerCloseButton />
           <DrawerHeader></DrawerHeader>
-
           <DrawerBody>
-            <Input placeholder="Type here..." />
+            <br />
+            <Flex p={{ base: "10px" }}>
+              <Heading
+                className="text-senju"
+                opacity={0.7}
+                fontSize={{ base: "lg" }}
+              >
+                Our Services
+              </Heading>
+            </Flex>
+
+            <Flex borderBottom={"0.1px solid gray"} />
+            <Flex p={{ base: "10px" }}>
+              <NavLink to={"/glim"} onClick={onClose}>
+                GLIM <span className="text-secondary"> (Manufacturing)</span>
+              </NavLink>
+            </Flex>
+            <Flex borderBottom={"0.1px solid gray"} />
+            <Flex p={{ base: "10px" }}>
+              <NavLink to={"/shield"} onClick={onClose}>
+                SHIELD <span className="text-secondary">(Manufacturing)</span>
+              </NavLink>
+            </Flex>
+            <Flex borderBottom={"0.1px solid gray"} />
+            <Flex p={{ base: "10px" }}>
+              <NavLink to={"/edtech"} onClick={onClose}>
+                EdTech
+              </NavLink>
+            </Flex>
+            <Flex borderBottom={"0.1px solid gray"} />
+            <Flex p={{ base: "10px" }}>
+              <NavLink to={"/marketing&events"} onClick={onClose}>
+                Marketing & Events
+              </NavLink>
+            </Flex>
+            <Flex borderBottom={"0.1px solid gray"} />
+            <br />
+            <Flex p={{ base: "10px" }}>
+              <Heading
+                className="text-senju"
+                opacity={0.7}
+                fontSize={{ base: "lg" }}
+              >
+                Our Products
+              </Heading>
+            </Flex>
+            <Flex borderBottom={"0.1px solid gray"} />
+            <Flex p={{ base: "10px" }}>
+              <NavLink to={"/arra"} onClick={onClose}>
+                ARRA
+              </NavLink>
+            </Flex>
+            <Flex borderBottom={"0.1px solid gray"} />
+            <Flex p={{ base: "10px" }}>
+              <NavLink to={"/metakalvi"} onClick={onClose}>
+                Metakalvi
+              </NavLink>
+            </Flex>
+            <Flex borderBottom={"0.1px solid gray"} />
+            <Flex p={{ base: "10px" }}>
+              <NavLink to={"/qualisence"} onClick={onClose}>
+                Qualisence
+              </NavLink>
+            </Flex>
+            <Flex borderBottom={"0.1px solid gray"} />
+            <Flex p={{ base: "10px" }}>
+              <NavLink to={"/virtaas"} onClick={onClose}>
+                VIRTaaS
+              </NavLink>
+            </Flex>
+            <br />
+            <Flex p={{ base: "10px" }}>
+              <Heading
+                className="text-senju"
+                opacity={0.7}
+                fontSize={{ base: "lg" }}
+              >
+                Others
+              </Heading>
+            </Flex>
+
+            <Flex borderBottom={"0.1px solid gray"} />
+            <Flex p={{ base: "10px" }}>
+              <NavLink to={"/"} onClick={onClose}>
+                Home
+              </NavLink>
+            </Flex>
+            <Flex borderBottom={"0.1px solid gray"} />
+            <Flex p={{ base: "10px" }}>
+              <NavLink to={"/casestudies"} onClick={onClose}>
+                Case Studies
+              </NavLink>
+            </Flex>
+            <Flex borderBottom={"0.1px solid gray"} />
+            <Flex p={{ base: "10px" }}>
+              <NavLink to={"/blogs"} onClick={onClose}>
+                Blogs
+              </NavLink>
+            </Flex>
+            <Flex borderBottom={"0.1px solid gray"} />
           </DrawerBody>
         </DrawerContent>
       </Drawer>
