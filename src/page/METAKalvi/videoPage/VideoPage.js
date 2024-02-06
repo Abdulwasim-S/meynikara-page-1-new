e78d5efd1ea6769d4c2ba6e1d5704d94a9821907
Abdulwasim-S <abdulwasimsmech@gmail.com>
2024-02-06 20:19:27 +0530

    update

--- src/page/METAKalvi/videoPage/VideoPage.js
+++ src/page/METAKalvi/videoPage/VideoPage.js
@@ -1,14 +1,140 @@
 import { Box, Button, Flex, Grid, GridItem } from "@chakra-ui/react";
 import React, { useState } from "react";
 import YouTube from "react-youtube";
+import vgb1 from "./vgb1.png";
+import vgb2 from "./vgb2.png";
 
 const VideoPage = () => {
   const [show, setShow] = useState(true);
   const [show2, setShow2] = useState(true);
   return (
-    <Flex w={"100%"} justify={"space-around"}>
-      <Box h={"40vh"} w={{ base: "", md: "49vw" }} bg={"red"}></Box>
-      <Box h={"40vh"} w={{ base: "", md: "49vw" }} bg={"red"}></Box>
+    <Flex
+      w={"100%"}
+      flexDirection={{ base: "column" }}
+      alignItems={"center"}
+      justify={"space-around"}
+      bg={"blue"}
+    >
+      <Box p={5} h={{ lg: "50vh" }} w={{ base: "70vw", md: "49vw" }} bg={"red"}>
+        {show && (
+          <Box
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            bgImage={vgb1}
+            h={"100%"}
+            w={"100%"}
+            bgSize={"cover"}
+            bgPosition={"center"}
+            borderRadius={{ base: "25px", lg: "30px" }}
+          >
+            <Button
+              transform={{ base: "", md: "scale(1.5)" }}
+              _hover={{ transform: "scale(1.3)", color: "#ffba00" }}
+              px={"20px"}
+              color={"#22c997"}
+              fontSize={"xl"}
+              onClick={() => {
+                setShow(false);
+              }}
+            >
+              <i className="fa-solid fa-play"></i>
+            </Button>
+          </Box>
+        )}
+        {!show && (
+          <YouTube
+            className="h-100 w-100"
+            videoId={"tf_BlmqlCTA"}
+            opts={{
+              width: "100%",
+              height: "100%",
+            }}
+            onPlay={() => {
+              // eslint-disable-next-line no-undef
+              gtag(
+                "event",
+                `${localStorage["meynikara-from-page"]} Video Started`,
+                {
+                  event_category: `${localStorage["meynikara-from-page"]} Video`,
+                  event_label: "SHIELD",
+                }
+              );
+            }}
+            onEnd={() => {
+              // eslint-disable-next-line no-undef
+              gtag(
+                "event",
+                `${localStorage["meynikara-from-page"]} Video Completed`,
+                {
+                  event_category: `${localStorage["meynikara-from-page"]} Video`,
+                  event_label: "shield",
+                }
+              );
+            }}
+          />
+        )}
+      </Box>
+      <Box p={5} h={"50vh"} w={{ base: "100%", md: "49vw" }} bg={"red"}>
+        {show2 && (
+          <Box
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            bgImage={vgb2}
+            h={"100%"}
+            w={"100%"}
+            bgSize={"cover"}
+            bgPosition={"center"}
+            borderRadius={{ base: "25px", lg: "30px" }}
+          >
+            <Button
+              transform={{ base: "", md: "scale(1.5)" }}
+              _hover={{ transform: "scale(1.3)", color: "#ffba00" }}
+              px={"20px"}
+              color={"#22c997"}
+              fontSize={"xl"}
+              onClick={() => {
+                setShow2(false);
+              }}
+            >
+              <i className="fa-solid fa-play"></i>
+            </Button>
+          </Box>
+        )}
+        {!show2 && (
+          <YouTube
+            className="h-100 w-100"
+            videoId={"tf_BlmqlCTA"}
+            opts={{
+              width: "100%",
+              height: "100%",
+            }}
+            onPlay={() => {
+              // eslint-disable-next-line no-undef
+              gtag(
+                "event",
+                `${localStorage["meynikara-from-page"]} Video Started`,
+                {
+                  event_category: `${localStorage["meynikara-from-page"]} Video`,
+                  event_label: "SHIELD",
+                }
+              );
+            }}
+            onEnd={() => {
+              // eslint-disable-next-line no-undef
+              gtag(
+                "event",
+                `${localStorage["meynikara-from-page"]} Video Completed`,
+                {
+                  event_category: `${localStorage["meynikara-from-page"]} Video`,
+                  event_label: "shield",
+                }
+              );
+            }}
+          />
+        )}
+      </Box>
     </Flex>
   );
 };
